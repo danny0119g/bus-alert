57254bcf99387e5b8439cb1c9cf7d236b221bff7
danny0119g <danny0119g@gmail.com>
2026-09-23 11:09:17 +0900

    임시 디버그용 기준선 추가

--- worker.js
+++ worker.js
@@ -260,8 +260,7 @@ function renderPage() {
     background: var(--blue);
     margin-right: 6px;
     box-shadow: 0 0 6px rgba(62,166,255,0.5);
-    position: relative;
-    top: 0.5px;
+    outline: 1px solid red;
   }
   .dot.error {
     background: var(--red);
@@ -272,7 +271,7 @@ function renderPage() {
     0%, 100% { opacity: 1; }
     50% { opacity: 0.3; }
   }
-  #statusText { color: var(--text-dim); line-height: 1; position: relative; top: 1px; }
+  #statusText { color: var(--text-dim); line-height: 1; outline: 1px solid lime; }
   #statusText.error { color: var(--red); }
   .status-group {
     display: flex;
